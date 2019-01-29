5b2ca91ab727b9cf4515dec1635fb9a227f60491
Ryan <supermarioryan@gmail.com>
2019-01-29 22:04:15 +0000

    > disable timeout

--- static/worker.js
+++ static/worker.js
@@ -17,7 +17,7 @@ self.addEventListener('install', function (evt) {
 self.addEventListener('fetch', function(evt) {
   console.log('The service worker is serving the asset.');
 
-  evt.respondWith(fromNetwork(evt.request, 2000).catch(function () {
+  evt.respondWith(fromNetwork(evt.request).catch(function () {
     return fromCache(evt.request);
   }));
 });
@@ -30,13 +30,8 @@ function precache() {
 
 function fromNetwork(request, timeout) {
   return new Promise(function (fulfill, reject) {
-
-    var timeoutId = setTimeout(reject, timeout);
-
     fetch(request).then(function (response) {
-      clearTimeout(timeoutId);
       fulfill(response);
-
     }, reject);
   });
 }
