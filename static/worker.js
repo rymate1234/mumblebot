dc7d17185a08e7ff58a1220fa02613e990e3ce20
Ryan <supermarioryan@gmail.com>
2019-01-29 21:54:22 +0000

    >further worker fixes

--- static/worker.js
+++ static/worker.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
-var CACHE = 'cache-and-update'
 var resources = [
   '/index.js'
 ]
+var CACHE = 'network-or-cache';
+
 // On install, cache some resources.
 self.addEventListener('install', function (evt) {
   caches.keys().then(function(names) {
@@ -10,35 +11,40 @@ self.addEventListener('install', function (evt) {
   });
 
   console.log('The service worker is being installed.')
-  // Ask the service worker to keep installing until the returning promise
-  // resolves.
-  evt.waitUntil(precache())
+  evt.waitUntil(precache());
 })
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
-  );
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+
+  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+    return fromCache(evt.request);
+  }));
 });
 
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
-function precache () {
+function precache() {
   return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(resources)
-  })
+    return cache.addAll(resources);
+  });
 }
 
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
-function fromCache (request) {
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+
+    var timeoutId = setTimeout(reject, timeout);
+
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+
+    }, reject);
+  });
+}
+
+function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
-      return matching || fetch(event.request)
-    })
-  })
+      return matching || Promise.reject('no-match');
+    });
+  });
 }
-
