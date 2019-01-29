c78b975f06b43e465877d03a826d333a24f1b422
Ryan <supermarioryan@gmail.com>
2019-01-29 21:38:12 +0000

    > Attempt to fix service worker

--- static/worker.js
+++ static/worker.js
@@ -5,25 +5,28 @@ var resources = [
 ]
 // On install, cache some resources.
 self.addEventListener('install', function (evt) {
+
+  navigator.serviceWorker.getRegistrations().then(
+    function(registrations) {
+      for(let registration of registrations) {  
+          registration.unregister();
+      }
+    }
+  );
+
   console.log('The service worker is being installed.')
   // Ask the service worker to keep installing until the returning promise
   // resolves.
   evt.waitUntil(precache())
 })
 
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
-self.addEventListener('fetch', function (evt) {
-  if (evt.request.url.includes(self.location.hostname) && evt.request.url.contains('index.js')) {
-    console.log('The service worker is serving the asset.')
-    // You can use `respondWith()` to answer immediately, without waiting for the
-    // network response to reach the service worker...
-    evt.respondWith(fromCache(evt.request))
-    // ...and `waitUntil()` to prevent the worker from being killed until the
-    // cache is updated.
-    evt.waitUntil(update(evt.request))
-  }
-})
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
 
 // Open a cache and use `addAll()` with an array of assets to add all of them
 // to the cache. Return a promise resolving when all the assets are added.
@@ -39,17 +42,8 @@ function precache () {
 function fromCache (request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match')
+      return matching || fetch(event.request)
     })
   })
 }
 
-// Update consists in opening the cache, performing a network request and
-// storing the new response data.
-function update (request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response)
-    })
-  })
-}
